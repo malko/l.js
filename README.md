b3153aee29630775aadb449afde063c3b7552db4
malko <jgotti@jgotti.org>
2014-05-25 01:59:08 +0200

    fallback support rewrite + null id bug correction + minification work

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
-# l.js is another simple/tiny javascript/css loader 
+# l.js is another simple/tiny javascript/css loader
 
 ## features
 - parrallel script / css loading
 - callback after script loading (css support callback too but are excuted imediately)
-- tiny only 1.9ko uglifyed, 0.9ko gziped (at least for now)
+- tiny only 2ko uglifyed, 1ko gziped (at least for latest revision)
 - may load in order to preserve dependencies
 - support aliases for simpler calling
 - on demand loading
 - only one script tag required
 - clear syntax
 - successive load of same file will load it once but execute all callbacks associated
-- can check already inserted tags at load time
+- can dumbly check already inserted tags at load time
+- may use a fallback url on error (only for js files and with error events compatible browsers)
 
 ## examples
 
@@ -34,8 +35,7 @@ second load will be executed in parrallel of first load but myDependentLib.js wo
 
 ```html
 <script src="l.js">
-	ljs.load(['myLib.js','myRequiredLib.js'],'myDependentLib.js',function(){ /* your callback here */})
-	;
+	ljs.load(['myLib.js','myRequiredLib.js'],'myDependentLib.js',function(){ /* your callback here */});
 </script>
 ```
 this will load myLib.js and myRequiredLib.js in parrallel and wait for them before loading myDependentLib.js
@@ -43,7 +43,7 @@ this will load myLib.js and myRequiredLib.js in parrallel and wait for them befo
 
 ### using some aliases for simpler loading
 ```html
-<script src="l.js?checkLoaded"> // <- adding checkLoaded to the url will check already inserted script/link tags
+<script src="l.js?checkLoaded"> // <- adding checkLoaded to the url will dumbly check already inserted script/link tags
 	ljs
 		.addAliases({
 			jQuery:'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js#jqueryId' // <- script tag will have attribute id=jqueryId
@@ -59,6 +59,18 @@ this will load myLib.js and myRequiredLib.js in parrallel and wait for them befo
 	;
 </script>
 ```
-	
+
+### define a fallback url
+l.js also support a fallback url for javascript files in case you want to try to get the resource from another location on loading failure
+You can define this fallback url parameter like you define ids. The difference is you will prefix with **#=** instead of # alone
+```html
+<script src="l.js">
+	ljs.load('http://domain.com/myLib.js#=/myfallback.js#myid',function(){
+		/*
+			generated script tag will have myid as id and will try to load /myfallback.js if it fail to load http://domain.com/myLib.js
+		*/
+	});
+</script>
+```
 this piece of code is dual licenced under MIT / GPL
 Hope this help, code review, suggestions, bug reports are welcome and appreciated.
--- l.js
+++ l.js
@@ -1,5 +1,5 @@
 //https://github.com/malko/l.js
-(function(){
+;(function(window, undefined){
 /*
 * script for js/css parallel loading with dependancies management
 * @author Jonathan Gotti < jgotti at jgotti dot net >
@@ -7,6 +7,7 @@
 * @since 2012-04-12
 * @todo add prefetching using text/cache for js files
 * @changelog
+*            - 2014-05-25 - fallback support rewrite + null id bug correction + minification work
 *            - 2014-05-21 - add cdn fallback support with hashbang url
 *            - 2014-05-22 - add support for relative paths for stylesheets in checkLoaded
 *            - 2014-05-21 - add support for relative paths for scripts in checkLoaded
@@ -15,6 +16,7 @@
 *            - 2012-04-20 - now sharp part of url will be used as tag id
 *                         - add options for checking already loaded scripts at load time
 *            - 2012-04-19 - add addAliases method
+* @note coding style is implied by the target usage of this script not my habbits
 */
 	/** gEval credits goes to my javascript idol John Resig, this is a simplified jQuery.globalEval */
 	var gEval = function(js){ ( window.execScript || function(js){ window[ "eval" ].call(window,js);} )(js); }
@@ -22,133 +24,126 @@
 		//-- some minifier optimisation
 		, D = document
 		, getElementsByTagName = 'getElementsByTagName'
-		, replace = 'replace'
-		, match = 'match'
 		, length = 'length'
 		, readyState = 'readyState'
 		, onreadystatechange = 'onreadystatechange'
 		//-- get the current script tag for further evaluation of it's eventual content
-		,scripts = D[getElementsByTagName]("script")
-		,script  = scripts[ scripts[length] - 1 ].innerHTML[replace](/^\s+|\s+$/g,'')
+		, scripts = D[getElementsByTagName]("script")
+		, scriptTag = scripts[scripts[length]-1]
+		, script  = scriptTag.innerHTML.replace(/^\s+|\s+$/g,'')
 	;
 	//avoid multiple inclusion to override current loader but allow tag content evaluation
-	if( typeof ljs !== 'undefined' ){ script && gEval(script); return; }
-
-	var checkLoaded = scripts[ scripts[length] - 1 ].src[match](/checkLoaded/)?true:false
-		//-- keep trace of header as we will make multiple access to it
-		,header  = D[getElementsByTagName]("head")[0] || D.documentElement
-    ,fallbackRetry =  function(error, callback){
-      var fallbackURL = error.currentTarget.id;
-      if(fallbackURL.indexOf('.js') !== -1) { //retry with hashbang url
-        console.error('ljs retrying '+fallbackURL);
-        ljs.load(fallbackURL, callback);
-      }
-    }
-		,appendElmt = function(type,attrs,cb){
-			var e = D.createElement(type), i;
-			if( cb ){ //-- this is not intended to be used for link
-				if(e[readyState]){
-					e[onreadystatechange] = function(){
-						if (e[readyState] === "loaded" || e[readyState] === "complete"){
-							e[onreadystatechange] = null;
-							cb();
+	if( ! window.ljs ){
+		var checkLoaded = scriptTag.src.match(/checkLoaded/)?1:0
+			//-- keep trace of header as we will make multiple access to it
+			,header  = D[getElementsByTagName]("head")[0] || D.documentElement
+			, urlParse = function(url){
+				var parts={}; // u => url, i => id, f = fallback
+				parts.u = url.replace(/#(=)?([^#]*)?/g,function(m,a,b){ parts[a?'f':'i'] = b; return '';});
+				return parts;
+			}
+			,appendElmt = function(type,attrs,cb){
+				var e = D.createElement(type), i;
+				if( cb ){ //-- this is not intended to be used for link
+					if(e[readyState]){
+						e[onreadystatechange] = function(){
+							if (e[readyState] === "loaded" || e[readyState] === "complete"){
+								e[onreadystatechange] = null;
+								cb();
+							}
+						};
+					}else{
+						e.onload = cb;
+					}
+				}
+				for( i in attrs ){ attrs[i] && (e[i]=attrs[i]); }
+				header.appendChild(e);
+				// return e; // unused at this time so drop it
+			}
+			,load = function(url,cb){
+				if( this.aliases && this.aliases[url] ){
+					var args = this.aliases[url].slice(0);
+					isA(args) || (args=[args]);
+					cb && args.push(cb);
+					return this.load.apply(this,args);
+				}
+				if( isA(url) ){ // parallelized request
+					for( var l=url[length]; l--;){
+						this.load(url[l]);
+					}
+					cb && url.push(cb); // relaunch the dependancie queue
+					return this.load.apply(this,url);
+				}
+				if( url.match(/\.css\b/) ){
+					return this.loadcss(url,cb);
+				}
+				return this.loadjs(url,cb);
+			}
+			,loaded = {}  // will handle already loaded urls
+			,loader  = {
+				aliases:{}
+				,loadjs: function(url,cb){
+					var parts = urlParse(url);
+					url = parts.u;
+					if( loaded[url] === true ){ // already loaded exec cb if any
+						cb && cb();
+						return this;
+					}else if( loaded[url]!== undefined ){ // already asked for loading we append callback if any else return
+						if( cb ){
+							loaded[url] = (function(ocb,cb){ return function(){ ocb && ocb(); cb && cb(); }; })(loaded[url],cb);
 						}
-					};
-				}else{
-					e.onload = cb;
+						return this;
+					}
+					// first time we ask this script
+					loaded[url] = (function(cb){ return function(){loaded[url]=true; cb && cb();};})(cb);
+					cb = function(){ loaded[url](); };
+					appendElmt('script',{type:'text/javascript',src:url,id:parts.i,onerror:function(error){
+						if( parts.f ){
+							var c = error.currentTarget;
+							c.parentNode.removeChild(c);
+							appendElmt('script',{type:'text/javascript',src:parts.f,id:parts.i},cb);
+						}
+					}},cb);
+					return this;
 				}
-			}
-			for( i in attrs ){ e[i]=attrs[i]; }
-			header.appendChild(e);
-			// return e; // unused at this time so drop it
-		}
-		,load = function(url,cb){
-			if( this.aliases && this.aliases[url] ){
-				var args = this.aliases[url].slice(0);
-				isA(args) || (args=[args]);
-				cb && args.push(cb);
-				return this.load.apply(this,args);
-			}
-			if( isA(url) ){ // parallelized request
-				for( var l=url.length; l--;){
-					this.load(url[l]);
-				}
-				cb && url.push(cb); // relaunch the dependancie queue
-				return this.load.apply(this,url);
-			}
-			if( url[match](/\.css\b/) ){
-				return this.loadcss(url,cb);
-			}
-			return this.loadjs(url,cb);
-		}
-		,loaded = {}  // will handle already loaded urls
-		,loader  = {
-			aliases:{}
-			,loadjs: function(url,cb){
-				var id  =(url[match]('#')?url[replace](/^[^#]+#/,''):null);
-				id && (url = url[replace](/#.*$/,''));
-				if( loaded[url] === true ){ // already loaded exec cb if any
+				,loadcss: function(url,cb){
+					var parts = urlParse(url);
+					url = parts.u;
+					loaded[url] || appendElmt('link',{type:'text/css',rel:'stylesheet',href:url,id:parts.i});
+					loaded[url] = true;
 					cb && cb();
 					return this;
-				}else if( loaded[url]!== undefined ){ // already asked for loading we append callback if any else return
-					if( cb ){
-						loaded[url] = function(ocb,cb){ return function(){ ocb && ocb(); cb && cb(); } }(loaded[url],cb);
+				}
+				,load: function(){
+					var argv=arguments,argc = argv[length];
+					if( argc === 1 && isA(argv[0],Function) ){
+						argv[0]();
+						return this;
+					}
+					load.call(this,argv[0], argc <= 1 ? undefined : function(){ loader.load.apply(loader,[].slice.call(argv,1));} );
+					return this;
+				}
+				,addAliases:function(aliases){
+					for(var i in aliases ){
+						this.aliases[i]= isA(aliases[i]) ? aliases[i].slice(0) : aliases[i];
 					}
 					return this;
 				}
-				// first time we ask this script
-				loaded[url] = function(cb){ return function(){loaded[url]=true; cb && cb();}}(cb);
-				appendElmt('script',{type:'text/javascript',src:url,id:id,onerror:function(error){
-          fallbackRetry(error, function(){ loaded[url]()})
-        }},function(){
-          loaded[url]()
-        });
-				return this;
 			}
-			,loadcss: function(url,cb){
-				var id  =(url[match]('#')?url[replace](/^[^#]+#/,''):null);
-				id && (url = url[replace](/#.*$/,''));
-				if(! loaded[url]){
-					appendElmt('link',{type:'text/css',rel:'stylesheet',href:url,id:id},function(){ loaded[url]=true; });
-				}
-				loaded[url] = true;
-				cb && cb();
-				return this;
+		;
+		if( checkLoaded ){
+			var i,l,links,url;
+			for(i=0,l=scripts[length];i<l;i++){
+				(url = scripts[i].getAttribute('src')) && (loaded[url.replace(/#.*$/,'')] = true);
 			}
-			,load: function(){
-				var argv=arguments,argc = argv[length];
-				if( argc === 1 && isA(argv[0],Function) ){
-					argv[0]();
-					return this;
-				}
-				load.call(this,argv[0], argc <= 1 ? undefined : function(){ loader.load.apply(loader,[].slice.call(argv,1))} )
-				return this;
-			}
-			,addAliases:function(aliases){
-				for(var i in aliases ){
-					this.aliases[i]= isA(aliases[i]) ? aliases[i].slice(0) : aliases[i];
-				}
-				return this;
+			links = D[getElementsByTagName]('link');
+			for(i=0,l=links[length];i<l;i++){
+				(links[i].rel==='stylesheet' || links[i].type==='text/css') && (loaded[links[i].getAttribute('href')]=true);
 			}
 		}
-	;
-	if( checkLoaded ){
-		var i,l,links;
-		for(i=0,l=scripts[length];i<l;i++){
-	      		var url = scripts[i].getAttribute('src');
-	      		if(url){
-      				var id  =(url[match]('#')?url[replace](/^[^#]+#/,''):null);
-      				id && (url = url[replace](/#.*$/,''));
-      				loaded[url]=true;
-	      		}
-		}
-		links = D[getElementsByTagName]('link');
-		for(i=0,l=links[length];i<l;i++){
-			(links[i].rel==="stylesheet" || links[i].type==='text/css') && (loaded[links[i].getAttribute('href')]=true);
-		}
+		//export ljs
+		window.ljs = loader;
+		// eval inside tag code if any
 	}
-	//export ljs
-	ljs = loader;
-	// eval inside tag code if any
 	script && gEval(script);
-})()
+})(window);
